a0635b9eb132c3107bb72a207319fc6ac79c8a6e
Michelle Yang <zomgitsmichelle@gmail.com>
2019-03-14 23:36:39 -0700

    Changed redirect-uri to a10 link

--- routes/render.js
+++ routes/render.js
@@ -92,8 +92,9 @@ exports.getSpotifyToken = async function(request, response) {
 	var body = {
 		grant_type: "authorization_code",
 		code: code,
-		redirect_uri: "https://pomelody.herokuapp.com/spotifyLogin"
-		//redirect_uri: "http://localhost:3000/spotifyLogin"
+		// redirect_uri: "https://pomelody.herokuapp.com/spotifyLogin"
+		// redirect_uri: "http://localhost:3000/spotifyLogin"
+		redirect_uri: "https://a10-pomelody.herokuapp.com/spotifyLogin"
 
 	}
 	paramed_body = param(body);
